17def4c52ee38907e3c2a5c5625c6e84f6c967d5
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-03-19 21:51:33 +0300

    Update casinoProject.cpp

--- casinoProject.cpp
+++ casinoProject.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <string.h>
 #include <math.h>
-#include <random>
+#include <string>
 #include <time.h>
+#include <stdlib.h>
+#include <ctime>
 using namespace std;
 int main(){
-
-
+     
    int kullaniciSayisi1,kullaniciSayisi2,kullaniciSayisi3; 
    int randomSayi1,randomSayi2,randomSayi3;
    int puan = 0;
@@ -16,304 +16,285 @@ int main(){
    int tamMaglubiyet = -2;
    int min = 1;
    int max = 10;
+
+   cout << "Birinci Sayiyi Giriniz : " ;
+
+   cin >> kullaniciSayisi1;
+
+   cout << "Ikinci Sayiyi Giriniz : " ;
+
+   cin >> kullaniciSayisi2;
+
+   cout << "Ucuncu Sayiyi Giriniz : " ;
+
+   cin >> kullaniciSayisi3;
+
+   cout << "  | ____?____ |   | ____?____ |   | ____?____ | " << endl;
+
+   srand(time(NULL) + clock());
+
+   randomSayi1 = rand() % (max - min + 1);
+   randomSayi2 = rand() % (max - min + 1);
+   randomSayi3 = rand() % (max - min + 1);
+
+   if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+     cout << "Kazandiniz!" << endl;
+
+     puan += tamGalibiyet;
+
+     cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+     cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+     cout << "Puan : " << puan << endl;
+     
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+    cout << "2/3 Yaptiniz!" << endl;
+
+    puan += yariGalibiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+    cout << "2/3 Yaptiniz!" << endl;
+
+    puan += yariGalibiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "2/3 Yaptiniz!" << endl;
+
+    puan += yariGalibiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+    cout << "1/3 Yaptiniz!" << endl;
+
+    puan += yariMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "1/3 Yaptiniz!" << endl;
+
+    puan += yariMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "1/3 Yaptiniz!" << endl;
+
+    puan += yariMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "Kaybettiniz!" << endl;
+
+    puan += tamMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+
+   while (true)
+   {
+   
+
+    cout << "Birinci Sayiyi Giriniz : " ;
+
+   cin >> kullaniciSayisi1;
+
+   cout << "Ikinci Sayiyi Giriniz : " ;
+
+   cin >> kullaniciSayisi2;
+
+   cout << "Ucuncu Sayiyi Giriniz : " ;
+
+   cin >> kullaniciSayisi3;
+
+   cout << "  | ____?____ |   | ____?____ |   | ____?____ | " << endl;
+
+   srand(time(NULL) + clock());
+
+   randomSayi1 = rand() % (max - min + 1);
+   randomSayi2 = rand() % (max - min + 1);
+   randomSayi3 = rand() % (max - min + 1);
+
+   if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+     cout << "Kazandiniz!" << endl;
+
+     puan += tamGalibiyet;
+
+     cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+     cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+     cout << "Puan : " << puan << endl;
+     
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+    cout << "2/3 Yaptiniz!" << endl;
+
+    puan += yariGalibiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+    cout << "2/3 Yaptiniz!" << endl;
+
+    puan += yariGalibiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "2/3 Yaptiniz!" << endl;
+
+    puan += yariGalibiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
+   {
+
+    cout << "1/3 Yaptiniz!" << endl;
+
+    puan += yariMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "1/3 Yaptiniz!" << endl;
+
+    puan += yariMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "1/3 Yaptiniz!" << endl;
+
+    puan += yariMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
+   {
+
+    cout << "Kaybettiniz!" << endl;
+
+    puan += tamMaglubiyet;
+
+    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
+
+    cout << "|    " << randomSayi1 << "     |" << "  " << "|    " << randomSayi2 << "     |" << "  " << "|    " << randomSayi3 << "    |" << endl; 
+
+    cout << "Puan : " << puan << endl;
+    
+   }
+
+    
+   }
   
-   char devam;
- 
  
 
-   cout << "Birinci Sayiyi Giriniz : " ;
+    return 0;
 
-   cin >> kullaniciSayisi1;
 
-   cout << "Ikinci Sayiyi Giriniz : " ;
 
-   cin >> kullaniciSayisi2;
-
-   cout << "Ucuncu Sayiyi Giriniz : " ;
-
-   cin >> kullaniciSayisi3;
-
-   cout << "  | ____?____ |   | ____?____ |   | ____?____ | " << endl;
-
-   srand(time(NULL) + clock());
-
-   randomSayi1 = rand() % (max - min + 1);
-   randomSayi2 = rand() % (max - min + 1);
-   randomSayi3 = rand() % (max - min + 1);
-
-   if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-     cout << "Kazandiniz!" << endl;
-
-     puan += tamGalibiyet;
-
-     cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-     cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-     cout << "Puan : " << puan << endl;
-     
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-    cout << "2/3 Yaptiniz!" << endl;
-
-    puan += yariGalibiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-    cout << "2/3 Yaptiniz!" << endl;
-
-    puan += yariGalibiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "2/3 Yaptiniz!" << endl;
-
-    puan += yariGalibiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-    cout << "1/3 Yaptiniz!" << endl;
-
-    puan += yariMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "1/3 Yaptiniz!" << endl;
-
-    puan += yariMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "1/3 Yaptiniz!" << endl;
-
-    puan += yariMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "Kaybettiniz!" << endl;
-
-    puan += tamMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-
-      
-   cout << "Devam Etmek Istiyor Musunuz : (E) , (H) " ;
-
-   cin >> devam;
-
-
-   while (devam == 'E'  ||  devam == 'e')  
-   {
-
-   cout << "Birinci Sayiyi Giriniz : " ;
-
-   cin >> kullaniciSayisi1;
-
-   cout << "Ikinci Sayiyi Giriniz : " ;
-
-   cin >> kullaniciSayisi2;
-
-   cout << "Ucuncu Sayiyi Giriniz : " ;
-
-   cin >> kullaniciSayisi3;
-
-   cout << "  | ____?____ |   | ____?____ |   | ____?____ | " << endl;
-
-   srand(time(NULL) + clock());
-
-   randomSayi1 = rand() % (max - min + 1);
-   randomSayi2 = rand() % (max - min + 1);
-   randomSayi3 = rand() % (max - min + 1);
-
-   if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-     cout << "Kazandiniz!" << endl;
-
-     puan += tamGalibiyet;
-
-     cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-     cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-     cout << "Puan : " << puan << endl;
-     
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-    cout << "2/3 Yaptiniz!" << endl;
-
-    puan += yariGalibiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-    cout << "2/3 Yaptiniz!" << endl;
-
-    puan += yariGalibiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "2/3 Yaptiniz!" << endl;
-
-    puan += yariGalibiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 == kullaniciSayisi3))
-   {
-
-    cout << "1/3 Yaptiniz!" << endl;
-
-    puan += yariMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 == kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "1/3 Yaptiniz!" << endl;
-
-    puan += yariMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 == kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "1/3 Yaptiniz!" << endl;
-
-    puan += yariMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-   else if ((randomSayi1 != kullaniciSayisi1) && (randomSayi2 != kullaniciSayisi2) && (randomSayi3 != kullaniciSayisi3))
-   {
-
-    cout << "Kaybettiniz!" << endl;
-
-    puan += tamMaglubiyet;
-
-    cout << "Sizin Tahminleriniz : " << "\t" << kullaniciSayisi1  << "\t" << kullaniciSayisi2  << "\t" << kullaniciSayisi3 << endl;
-
-    cout << "Cevaplar : " << randomSayi1  << "\t" << randomSayi2  << "\t" << randomSayi3 << endl;
-
-    cout << "Puan : " << puan << endl;
-    
-   }
-
-      
-   cout << "Devam Etmek Istiyor Musunuz : (E) , (H) " ;
-
-   cin >> devam;
-
-   }
-   
-   cout << "Puan : " << puan  << endl;  
-   
-       
-     
-
-      
-       
-      
-      
-
-
- 
 
 }
